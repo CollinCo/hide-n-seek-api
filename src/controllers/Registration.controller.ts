c0ff99ad290906e51ca9a6f8d9ee909b574a215b
Unknown <malcolmcarelse@gmail.com>
2018-07-09 16:50:28 +0200

    add jwt to controllers

--- src/controllers/Registration.controller.ts
+++ src/controllers/Registration.controller.ts
@@ -2,12 +2,13 @@ import { repository } from '@loopback/repository';
 import { UserRepository } from '../repositories/user.repository';
 import { User } from '../models';
 import { HttpErrors, post, requestBody } from '@loopback/rest';
+import { sign } from 'jsonwebtoken';
 
 export class RegistrationController {
   constructor(@repository(UserRepository) protected userRepo: UserRepository) { }
 
   @post('/registration')
-  async registerUser(@requestBody() user: User): Promise<User> {
+  async registerUser(@requestBody() user: User) {
     // Check that required fields are supplied
     if (!user.email || !user.password) {
       throw new HttpErrors.BadRequest('missing data');
@@ -22,6 +23,18 @@ export class RegistrationController {
       throw new HttpErrors.BadRequest('user already exists');
     }
 
-    return await this.userRepo.create(user);
+    let createdUser = await this.userRepo.create(user);
+
+    let jwt = sign({
+      user: createdUser
+    },
+      'shh', {
+        issuer: 'auth.hns.co.za',
+        audience: 'hns.co.za'
+      });
+
+    return {
+      token: jwt
+    };
   }
 }
