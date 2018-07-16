e09064708c71a43b310906a55e8897e08bc2cfcb
Jonah Deich <jonahdeich@gmail.com>
2018-07-16 15:03:53 +0200

    fixed stuff

--- migrations/20180711122526-game-tb.js
+++ migrations/20180711122526-game-tb.js
@@ -8,15 +8,15 @@ var seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
     dbm = options.dbmigrate;
     type = dbm.dataType;
     seed = seedLink;
 };
 
-exports.up = function(db, callback) {
+exports.up = function (db, callback) {
 
-    db.createTable('game', {
+    db.createTable('game_table', {
         uid: {
             type: 'int',
             primaryKey: true,
@@ -44,8 +44,8 @@ exports.up = function(db, callback) {
     }, callback);
 };
 
-exports.down = function(db, callback) {
-    db.dropTable('game', callback)
+exports.down = function (db, callback) {
+    db.dropTable('game_table', callback)
 };
 
 exports._meta = {
